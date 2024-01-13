80e6a4efb88bc469ec52169536edb07c9639240c
SanderMar <sander.marinus@student.uantwerpen.be>
2024-01-13 23:38:51 +0100

    Add basic first ingredient searching

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
 import lucene
 import index
+import search
 
 lucene.initVM()
 
 if __name__ == "__main__":
-    index.index_data("./data/recipes.parquet")
+    # index.index_data("./data/recipes.parquet")
+
+    searcher = search.get_searcher(search.get_index_dir())
+
+    hits = search.query_ingredients(searcher, ["milk", "eggs"])
+    print(len(hits))
+    storedFields = searcher.storedFields()
+    for hit in hits:
+        hitDoc = storedFields.document(hit.doc)
+        print(hitDoc.get("Name"))
+        print(hit.score, hitDoc.getValues("RecipeIngredientParts"))
