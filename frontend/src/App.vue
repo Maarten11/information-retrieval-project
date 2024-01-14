eaece53ea04b3b0cff95137f22f5f8e2b6146f8b
Maarten <maarten.peirsman@gmail.com>
2024-01-14 05:53:14 +0100

    Make a frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+	import { Ref, ref } from "vue";
+	import Recipe, { IRecipe } from "./components/Recipe.vue";
+	import imgUrl from "./assets/recipe_placeholder.jpg";
+
+	const elements: Ref<IRecipe[] | null> = ref(null);
+	const search = async () => {
+		const form = new FormData();
+		form.append("include", JSON.stringify(["apple"]));
+		const request = await fetch("http://localhost:8000/search_recipe", {
+			body: form,
+			method: "POST",
+		});
+		const response = await request.json();
+		elements.value = response;
+	};
+	const selectedRecipe: Ref<IRecipe | undefined> = ref(undefined);
+	const select = (recipe: IRecipe | undefined) => {
+		console.log("Selecting");
+		selectedRecipe.value = recipe;
+	};
+</script>
+
+<template>
+	<h1 class="text-3xl">Recipe Searcher</h1>
+	<button @click="search">Search</button>
+	<div class="grid gap-5">
+		<Recipe
+			class="hover:cursor-pointer"
+			@click="select(recipe)"
+			v-bind="recipe"
+			v-for="(recipe, index) in elements"
+			:key="index" />
+	</div>
+	<div
+		v-show="!!selectedRecipe"
+		class="fixed m-auto top-0 left-0 h-full w-full p-20 backdrop-blur-md">
+		<div class="relative h-full">
+			<div
+				class="grid grid-flow-row shadow-lg bg-stone-800 h-full overflow-y-auto rounded-2xl">
+				<img
+					class="w-full h-[30vh] object-cover"
+					:src="selectedRecipe?.Images ?? imgUrl" />
+				<button class="absolute top-0 left-0" @click="select(undefined)">
+					x
+				</button>
+				<div class="grid p-10 grid-flow-row gap-5">
+					<h2 class="text-3xl">{{ selectedRecipe?.Name }}</h2>
+					<div class="grid grid-flow-col gap-5">
+						<aside>
+							<h4 class="text-xl">Ingredients:</h4>
+							<ul class="text-left">
+								<li
+									v-for="ingredient in selectedRecipe?.RecipeIngredientParts"
+									:key="ingredient">
+									{{ ingredient }}
+								</li>
+							</ul>
+						</aside>
+						<section class="text-left">
+							<h4 class="text-xl">Instructions:</h4>
+							<ol>
+								<li v-for="instr in selectedRecipe?.RecipeInstructions">
+									{{ instr }}
+								</li>
+							</ol>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/Recipe.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export interface IRecipe {
+		Images?: string;
+		Name: string;
+		RecipeIngredientParts: Array<string>;
+		RecipeInstructions: Array<string>;
+	}
+</script>
+<script setup lang="ts">
+	import imgUrl from "../assets/recipe_placeholder.jpg";
+	const props = defineProps<IRecipe>();
+</script>
+
+<template>
+	<div
+		class="shadow-lg bg-stone-800 bg-opacity-35 overflow-hidden rounded-md grid grid-cols-[auto,1fr] gap-5">
+		<img
+			class="aspect-square max-h-24 object-cover overflow-hidden"
+			:src="props.Images ?? imgUrl"
+			:alt="Name" />
+		<div class="grid grid-flow-row text-left">
+			<h3 class="text-md">{{ props.Name }}</h3>
+			<!-- <ul>
+				<li
+					v-for="(ingredient, index) in props.RecipeIngredientParts"
+					:key="index">
+					{{ ingredient }}
+				</li>
+			</ul> -->
+			<span class="text-sm">{{ props.RecipeIngredientParts.join(", ") }}</span>
+		</div>
+	</div>
+</template>
